ac4ed2b65aaebdfc55dd76e7d1598084bde97bf0
Matthew McNicol <matthew.mcnicol@gmail.com>
2025-03-19 03:53:13 +0000

    added tests for sanitizeUserInput

---
+++ main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"strings"
+    "testing"
+)
+
+func TestSanitizeUserInput(t *testing.T) {
+    type test struct {
+        input string
+        want  string
+        scenario string
+    }
+
+    tests := []test{
+        {input: "user", want: "user", scenario: "no change"},
+        {input: strings.Repeat("x", 1000), want: strings.Repeat("x", 1000), scenario: "string length not shortened"},
+        {input: " user ", want: "user", scenario: "trim white space"},
+        {input: "<script>alert('boo')</script>", want: "&lt;script&gt;alert(&#39;boo&#39;)&lt;/script&gt;", scenario: "security"},
+        {input: strings.Repeat("x", 1001), want: strings.Repeat("x", 1000), scenario: "string length shortened"},
+    }
+
+    for _, tc := range tests {
+        got := sanitizeUserInput(tc.input)
+        if tc.want != got {
+            t.Fatalf("scenario %s: expected: %v, got: %v", tc.scenario, tc.want, got)
+        }
+    }
+}
